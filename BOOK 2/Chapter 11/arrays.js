83d46e73087eeecbe8e774234df2bc1ed368e437
Fuzzy8326 <139906105+Fuzzy8326@users.noreply.github.com>
2025-06-16 19:44:27 +0200

    Add files via upload

---
+++ BOOK 2/Chapter 11/arrays.js
@@ -0,0 +1,36 @@
+var people = ["Mary", "Bobby", "Judy", "Eddie", "Herbie", "Paul", "Bobby"];
+var otherPeople = ["Teddy", "Goergie", "Katty", "Jack"];
+
+
+//people[0] = "Georgie";
+
+//people = people.concat(otherPeople);
+
+//people = people.indexOf("Eddie");
+
+//people = people.join(" # ");
+
+//people = people.lastIndexOf("Bobby");
+
+//people = people.pop();
+//people.pop();
+
+//people = people.push("Teddy");
+//people.push("Teddy");
+
+//people = people.reverse();
+//people.shift();
+
+//people.unshift("Teddy");
+
+people = people.slice(1,5); 
+people = people.sort();
+
+people.splice(1,0,"Cathy");
+
+
+document.getElementById("peopleIKnow").innerHTML = people.valueOf();
+
+console.log(people)
+
+
